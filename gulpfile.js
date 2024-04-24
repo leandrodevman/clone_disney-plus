b5279c73c81c62f67f915ab8329df3edc03c4483
Leandro Ramos <leandrodeadkiller2018@gmail.com>
2024-04-24 18:14:17 -0300

    Configurando o uglify

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,14 @@ const gulp = require('gulp');
 // importando o gulp
 const sass = require('gulp-sass')(require('sass'));
 // importando sass e o gulp-sass
-const imagemin = require('gulp-imagemin')
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'));
+}
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -21,9 +28,10 @@ function images() {
         .pipe(gulp.dest('./dist/images'));
         // pasta de destino dos arquivos comprimidos
 }
-exports.default = gulp.parallel(styles, images);   
+exports.default = gulp.parallel(styles, images, scripts);   
 // função padrão para rodar os arquivos
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
 }
 // para observar e atualizar as mudanças de forma paralela
